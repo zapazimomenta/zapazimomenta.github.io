ebe47e9d38d7309c84c1faaaafe5deb20e29adf5
Nikolay Srebrev <nikolay@casecoach.com>
2024-03-13 00:14:43 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'alcohol';
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  alcohols = ['бира', 'водка', 'уиски', 'вино', 'джин', 'други'];
 }
