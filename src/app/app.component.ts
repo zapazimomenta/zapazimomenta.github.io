1b111d75bc4095edc96cbf39c17b8ded00e9d5d2
Nikolay Srebrev <nikolay@casecoach.com>
2024-03-13 20:43:45 +0200

    alcohol calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+
+interface Alcohol {
+  name: string;
+  alcPer: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,5 +12,29 @@ import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 })
 export class AppComponent {
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
-  alcohols = ['бира', 'водка', 'уиски', 'вино', 'джин', 'други'];
+  alcohols: Array<Alcohol> = [
+    { name: 'бира', alcPer: 4 },
+    { name: 'водка', alcPer: 40 },
+    { name: 'уиски', alcPer: 40 },
+    { name: 'вино', alcPer: 12 },
+    { name: 'джин', alcPer: 40 },
+    { name: 'други', alcPer: 40 },
+  ];
+  selectedAlcohol: Alcohol | null;
+  selectedAmount: number | null;
+  showResult: boolean = false;
+
+  get result(): string {
+    return (
+      ((this.selectedAmount as number) *
+        (this.selectedAlcohol as Alcohol).alcPer) /
+      1000
+    ).toFixed(2);
+  }
+
+  tryAgain() {
+    this.selectedAlcohol = null;
+    this.selectedAmount = null;
+    this.showResult = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    NgbModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgbModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
